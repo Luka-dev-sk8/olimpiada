841def05a00c5cf90a3087fd1cc671e6047b055f
Luka-dev-sk8 <lukantecao@gmail.com>
2025-06-14 21:08:19 -0300

    cambiazo

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-const boton = document.getElementById('mostrar_datos');
+/**const boton = document.getElementById('mostrar_datos');
         const caja = document.getElementById('caja_datos');
 
         boton.addEventListener('click', function(event) {
@@ -13,5 +13,23 @@ const boton = document.getElementById('mostrar_datos');
         document.addEventListener('click', function(event) {
             if (!caja.contains(event.target) && event.target !== boton) {
                 caja.style.display = 'none';
+            }
+        });
+**/
+const btn = document.getElementById('barra_lateral');
+const cajaL = document.getElementById('caja_desplegable');
+
+        btn.addEventListener('click', function(event) {
+            event.stopPropagation();
+            if (cajaL.style.display === 'none') {
+                cajaL.style.display = 'flex';
+            } else {
+                cajaL.style.display = 'none';
+            }
+        });
+
+        document.addEventListener('click', function(event) {
+            if (!cajaL.contains(event.target) && event.target !== btn) {
+                cajaL.style.display = 'none';
             }
         });
